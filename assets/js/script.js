33c3142d886e7a2244618a5969475e3edef0d72c
Adrian <adrianhenry2115@gmail.com>
2022-02-05 00:20:08 -0500

    feature for dates and urgency

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
 var tasks = {};
 
 var auditTask = function (taskEl) {
+  // get date from task element
+  var date = $(taskEl). find("span").text().trim();
   // to ensure element is getting to the function
-  console.log(taskEl);
+  console.log(date);
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning")
+  }
+  
 };
 
 var createTask = function (taskText, taskDate, taskList) {
@@ -248,6 +264,9 @@ $(".list-group").on("change", "input[type='text']", function () {
     .addClass("badge badge-primary badge-pill")
     .text(date);
   $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // remove all tasks
